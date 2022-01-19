d79fa0d49ab26ab1a41bf2436bf2738d7a38aeed
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-01-19 18:01:01 +0100

    Added User entity, repository and security (for user provider). Included a test controller to show UserRepository data. Added missing LICENSE.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(
+        UserRepository $userRepository
+    ): Response {
+        $users = $userRepository->findAll();
+        dump($users);
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
