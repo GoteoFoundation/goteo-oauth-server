dc54505e53390be796a885b263656038f84f38a8
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-02-02 16:57:13 +0100

    Created new endpoint to allow authenticating a user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use App\Service\PasswordValidatorService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,7 +23,7 @@ class HomeController extends AbstractController
         foreach ($users as $user) {
             $isValidPassword = $passwordValidatorService->isPasswordValid(
                 $user->getPassword(),
-                "TEST-PASSWORD"
+                "TEST_PASSWORD"
             );
 
             if ($isValidPassword) {
@@ -32,5 +34,33 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/login', name: 'login')]
+    public function loginAction(
+        Request $request,
+        UserRepository $userRepository,
+    ): Response {
+        $passwordValidatorService = new PasswordValidatorService();
+        $userEmail = $request->get("email");
+        $userPlainPassword = $request->get("password");
+
+        $user = $userRepository->findOneBy(['email' => $userEmail]);
+
+        if ($user == null) {
+            throw new Exception("There's no user with that email");
+        }
+
+        $isValid = $passwordValidatorService->isPasswordValid(
+            $user->getPassword(),
+            $userPlainPassword
+        );
+
+        if ($isValid) {
+            //TODO Authenticate user
+        }
     }
 }
