2be5a95cecb5dc7806bd0a7b4d8a758c3b87b020
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-02-02 16:42:37 +0100

    Added service that verifies user passwords

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Service\PasswordValidatorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,22 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(
-        UserRepository $userRepository
+        UserRepository $userRepository,
     ): Response {
+        $passwordValidatorService = new PasswordValidatorService();
         $users = $userRepository->findAll();
         dump($users);
+
+        foreach ($users as $user) {
+            $isValidPassword = $passwordValidatorService->isPasswordValid(
+                $user->getPassword(),
+                "TEST-PASSWORD"
+            );
+
+            if ($isValidPassword) {
+                dump("Password valid for: " . $user->getEmail());
+            }
+        }
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
