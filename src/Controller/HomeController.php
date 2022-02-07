6ab8f662ef7fadd766fcb6b16539620aff9750b0
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-02-07 13:46:28 +0100

    Remove user password validation from HomeController home action

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
-use App\Service\PasswordValidatorService;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,20 +13,8 @@ class HomeController extends AbstractController
     public function index(
         UserRepository $userRepository,
     ): Response {
-        $passwordValidatorService = new PasswordValidatorService();
         $users = $userRepository->findAll();
         dump($users);
-
-        foreach ($users as $user) {
-            $isValidPassword = $passwordValidatorService->isPasswordValid(
-                $user->getPassword(),
-                "TEST_PASSWORD"
-            );
-
-            if ($isValidPassword) {
-                dump("Password valid for: " . $user->getEmail());
-            }
-        }
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
