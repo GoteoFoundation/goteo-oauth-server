3ce5579c8bdd2b4d7c23ddb20b445b064fa82da1
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-02-14 15:26:52 +0100

    Finished implementation of OAuth with credentials so a user can authenticate and request an access token at the same time

---
+++ src/EventListener/UserResolveListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Repository\UserRepository;
+use App\Service\PasswordValidatorService;
+use League\Bundle\OAuth2ServerBundle\Event\UserResolveEvent;
+
+final class UserResolveListener
+{
+    private UserRepository $userRepository;
+    private PasswordValidatorService $passwordValidator;
+
+    public function __construct(UserRepository $userRepository, PasswordValidatorService $passwordValidator)
+    {
+        $this->userRepository = $userRepository;
+        $this->passwordValidator = $passwordValidator;
+    }
+
+    public function onUserResolve(UserResolveEvent $event): void
+    {
+        $user = $this->userRepository->findOneBy(['email' => $event->getUsername()]);
+
+        if ($user && $this->passwordValidator->isPasswordValid($user->getPassword(), $event->getPassword())) {
+            $event->setUser($user);
+        }
+    }
+}
