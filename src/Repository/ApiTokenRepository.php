3d59c20717452dcf34158666daeda392c374cb03
Carlos Martínez Gadea <carlosmartinezgadea@gmail.com>
2022-07-29 11:20:01 +0200

    Remove commented example methods at ApiTokenRepository

--- src/Repository/ApiTokenRepository.php
+++ src/Repository/ApiTokenRepository.php
@@ -46,33 +46,4 @@ class ApiTokenRepository extends ServiceEntityRepository
             $this->_em->flush();
         }
     }
-
-    // /**
-    //  * @return ApiToken[] Returns an array of ApiToken objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?ApiToken
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
